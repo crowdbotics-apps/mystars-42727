290564fd0d3418c4e9438b5f33d260fc5220e3b8
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-08-08 17:01:50 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI2/index.js
+++ screens/ScreenAI2/index.js
@@ -1,41 +1,53 @@
-import React from 'react';
-import { SafeAreaView, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, TouchableOpacity, FlatList, StyleSheet, Image } from "react-native";
 const zodiacSigns = [{
-  name: 'Aries',
-  symbol: '♈'
+  name: "Aries",
+  symbol: "♈",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Taurus',
-  symbol: '♉'
+  name: "Taurus",
+  symbol: "♉",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Gemini',
-  symbol: '♊'
+  name: "Gemini",
+  symbol: "♊",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Cancer',
-  symbol: '♋'
+  name: "Cancer",
+  symbol: "♋",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Leo',
-  symbol: '♌'
+  name: "Leo",
+  symbol: "♌",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Virgo',
-  symbol: '♍'
+  name: "Virgo",
+  symbol: "♍",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Libra',
-  symbol: '♎'
+  name: "Libra",
+  symbol: "♎",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Scorpio',
-  symbol: '♏'
+  name: "Scorpio",
+  symbol: "♏",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Sagittarius',
-  symbol: '♐'
+  name: "Sagittarius",
+  symbol: "♐",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Capricorn',
-  symbol: '♑'
+  name: "Capricorn",
+  symbol: "♑",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Aquarius',
-  symbol: '♒'
+  name: "Aquarius",
+  symbol: "♒",
+  image: "https://tinyurl.com/42evm3m3"
 }, {
-  name: 'Pisces',
-  symbol: '♓'
+  name: "Pisces",
+  symbol: "♓",
+  image: "https://tinyurl.com/42evm3m3"
 }];
 
 const ZodiacSignsScreen = ({
@@ -43,9 +55,12 @@ const ZodiacSignsScreen = ({
 }) => {
   const renderZodiacSign = ({
     item
-  }) => <TouchableOpacity style={styles.zodiacSignContainer} onPress={() => navigation.navigate('SignDetails', {
+  }) => <TouchableOpacity style={styles.zodiacSignContainer} onPress={() => navigation.navigate("SignDetails", {
     sign: item
   })}>
+      <Image source={{
+      uri: item.image
+    }} style={styles.zodiacSignImage} />
       <Text style={styles.zodiacSignName}>{item.name}</Text>
       <Text style={styles.zodiacSignSymbol}>{item.symbol}</Text>
     </TouchableOpacity>;
@@ -58,19 +73,36 @@ const ZodiacSignsScreen = ({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     paddingHorizontal: 16
   },
   zodiacSignContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingVertical: 12,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
+    borderBottomColor: "#ccc",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 12
+  },
+  zodiacSignImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 8
   },
   zodiacSignName: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginRight: 8
   },
   zodiacSignSymbol: {
--- screens/ScreenAI6/index.js
+++ screens/ScreenAI6/index.js
@@ -1,35 +1,100 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ZodiacScreen = () => {
-  const [day, setDay] = useState('');
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [zodiacSign, setZodiacSign] = useState('');
+  const [day, setDay] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [zodiacSign, setZodiacSign] = useState("");
 
   const handleValidateDate = () => {
     const isValid = validateDate(day, month, year);
 
     if (isValid) {
-      setErrorMessage('');
+      setErrorMessage("");
       const sign = getZodiacSign(day, month);
       setZodiacSign(sign);
     } else {
-      setErrorMessage('Invalid date');
-      setZodiacSign('');
+      setErrorMessage("Invalid date");
+      setZodiacSign("");
     }
   };
 
   const validateDate = (day, month, year) => {
-    // Date validation logic here
-    return true; // Placeholder logic, replace with actual validation
+    const isValidDay = /^\d{1,2}$/.test(day);
+    const isValidMonth = /^\d{1,2}$/.test(month);
+    const isValidYear = /^\d{4}$/.test(year);
+
+    if (!isValidDay || !isValidMonth || !isValidYear) {
+      return false;
+    }
+
+    const numericDay = parseInt(day, 10);
+    const numericMonth = parseInt(month, 10);
+    const numericYear = parseInt(year, 10);
+
+    if (numericMonth < 1 || numericMonth > 12) {
+      return false;
+    }
+
+    if (numericDay < 1 || numericDay > 31) {
+      return false;
+    }
+
+    if ((numericMonth === 4 || numericMonth === 6 || numericMonth === 9 || numericMonth === 11) && numericDay > 30) {
+      return false;
+    }
+
+    if (numericMonth === 2) {
+      if (numericDay > 29) {
+        return false;
+      }
+
+      if (numericYear % 4 === 0 && numericYear % 100 !== 0 || numericYear % 400 === 0 && numericYear % 100 === 0) {
+        if (numericDay > 29) {
+          return false;
+        }
+      } else {
+        if (numericDay > 28) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   };
 
   const getZodiacSign = (day, month) => {
-    // Zodiac sign calculation logic here
-    return 'Leo'; // Placeholder logic, replace with actual calculation
+    const numericDay = parseInt(day, 10);
+    const numericMonth = parseInt(month, 10);
+
+    if (numericMonth === 1 && numericDay >= 20 || numericMonth === 2 && numericDay <= 18) {
+      return "Aquarius";
+    } else if (numericMonth === 2 && numericDay >= 19 || numericMonth === 3 && numericDay <= 20) {
+      return "Pisces";
+    } else if (numericMonth === 3 && numericDay >= 21 || numericMonth === 4 && numericDay <= 19) {
+      return "Aries";
+    } else if (numericMonth === 4 && numericDay >= 20 || numericMonth === 5 && numericDay <= 20) {
+      return "Taurus";
+    } else if (numericMonth === 5 && numericDay >= 21 || numericMonth === 6 && numericDay <= 20) {
+      return "Gemini";
+    } else if (numericMonth === 6 && numericDay >= 21 || numericMonth === 7 && numericDay <= 22) {
+      return "Cancer";
+    } else if (numericMonth === 7 && numericDay >= 23 || numericMonth === 8 && numericDay <= 22) {
+      return "Leo";
+    } else if (numericMonth === 8 && numericDay >= 23 || numericMonth === 9 && numericDay <= 22) {
+      return "Virgo";
+    } else if (numericMonth === 9 && numericDay >= 23 || numericMonth === 10 && numericDay <= 22) {
+      return "Libra";
+    } else if (numericMonth === 10 && numericDay >= 23 || numericMonth === 11 && numericDay <= 21) {
+      return "Scorpio";
+    } else if (numericMonth === 11 && numericDay >= 22 || numericMonth === 12 && numericDay <= 21) {
+      return "Sagittarius";
+    } else {
+      return "Capricorn";
+    }
   };
 
   return <SafeAreaView style={styles.container}>
@@ -48,35 +113,35 @@ const ZodiacScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff"
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20
   },
   inputContainer: {
-    width: '80%',
-    alignItems: 'center'
+    width: "80%",
+    alignItems: "center"
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 40,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     marginBottom: 10,
     paddingHorizontal: 10
   },
   error: {
-    color: 'red',
+    color: "red",
     marginTop: 10
   },
   zodiac: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10
   }
 });
